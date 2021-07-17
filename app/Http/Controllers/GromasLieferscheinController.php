be0513432507abdfa0893d6808e167fc643b513b
BAHbKA007 <peniskopfbacken@gmail.com>
2021-07-17 12:51:16 +0200

    asd

--- app/Http/Controllers/GromasLieferscheinController.php
+++ app/Http/Controllers/GromasLieferscheinController.php
@@ -8,6 +8,6 @@ class GromasLieferscheinController extends Controller
 {
     public function push_to_database(Request $request)
     {
-        return var_dump($request);
+        return var_dump(json_decode($request));
     }
 }
